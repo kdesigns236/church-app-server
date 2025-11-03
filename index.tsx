f8ed85414bc5d9b6d0e794dde224eac95d53d1b5
kdesigns236 <kdesigns236@github.com>
2025-11-03 13:32:17 +0300

    Fix CSS bundling for offline support - v2.1.0 ready

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './src/index.css'; // Import Tailwind CSS
+import './index.css';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import { AppProvider } from './context/AppContext';
